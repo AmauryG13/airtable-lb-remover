6d1188f3af33d1e49541416a4df836d9480b07e8
Amaury Guillermin <amauryguillermin@gmail.com>
2020-04-20 14:23:50 +0200

    Select python exc in launcher

--- launcher/airtable-convertor.bat
+++ launcher/airtable-convertor.bat
@@ -1,7 +1,18 @@
 @echo off
 
+rem Set executable name
 SET EXEC=airtable-convertor
 SET MODULE=airtor.cli
+
+rem Get the python exec
+FOR /f %%i IN ('python --version') DO SET PY_VERS=%%i
+SET PY_V=%PY_VERS:~7,1%
+
+IF %PY_V% == "3" (
+    SET PYTHON_EXEC="python"
+) ELSE (
+    SET PYTHON_EXEC="python3"
+)
 
 CALL %EXEC% || GOTO:python_module
 
@@ -9,4 +20,4 @@ PAUSE 5
 GOTO:EOF
 
 :python_module
-CALL python3 -m %MODULE%
+CALL %PYTHON_EXEC% -m %MODULE%
