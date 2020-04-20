ce5d908020d911f0fb5d6c06f3a6329021d887af
Amaury Guillermin <amauryguillermin@gmail.com>
2020-04-20 14:32:34 +0200

    New permissions x2

--- launcher/airtable-convertor.sh
+++ launcher/airtable-convertor.sh
@@ -6,11 +6,11 @@ MODULE='airtor.cli'
 # Select python exec
 PY_VERS=$(python --version 2>&1)
 
-if [[ -n "${PY_VERS}" ]]; then
+if [ -n "${PY_VERS}" ]; then
   PY_V="${PY_VERS:7:1}"
 fi
 
-if [[ "${PY_V}" == "3" ]]; then
+if [ "${PY_V}" == "3" ]; then
   PYTHON_EXEC="python"
 else
   PYTHON_EXEC="python3"
