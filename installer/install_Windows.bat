5dfa9c639ec12d5e4171c0a7ab3e674701e89524
Amaury Guillermin <amauryguillermin@gmail.com>
2020-04-20 15:32:28 +0200

    Update windows exec file

--- installer/install_Windows.bat
+++ installer/install_Windows.bat
@@ -1,12 +1,20 @@
+ECHO @OFF
+
 rem Setup path
 SET BaseDir=%CD%
-SET SetupDir=%BaseDir%\..
+SET SetupDir="%BaseDir%\.."
+
+echo %SetupDir%
 
 rem Get the python exec
-FOR /f %%i IN ('python --version') DO SET PY_VERS=%%i
+FOR /f "delims=" %%i IN ('python --version') DO SET PY_VERS=%%i
+echo %PY_VERS%
+
 SET PY_V=%PY_VERS:~7,1%
 
-IF %PY_V% == "3" (
+echo %PY_V%
+
+IF /I "%PY_V%" == "3" (
     SET PYTHON_EXEC="python"
 ) ELSE (
     SET PYTHON_EXEC="python3"
@@ -15,3 +23,5 @@ IF %PY_V% == "3" (
 rem Build package
 CALL cd %SetupDir%
 CALL %PYTHON_EXEC% setup.py install --user
+
+PAUSE 5
--- launcher/airtable-convertor.bat
+++ launcher/airtable-convertor.bat
@@ -5,19 +5,15 @@ SET EXEC=airtable-convertor
 SET MODULE=airtor.cli
 
 rem Get the python exec
-FOR /f %%i IN ('python --version') DO SET PY_VERS=%%i
+FOR /f "delims=" %%i IN ('python --version') DO SET PY_VERS=%%i
 SET PY_V=%PY_VERS:~7,1%
 
-IF %PY_V% == "3" (
+IF /I "%PY_V%" == "3" (
     SET PYTHON_EXEC="python"
 ) ELSE (
     SET PYTHON_EXEC="python3"
 )
 
-CALL %EXEC% || GOTO:python_module
+CALL %PYTHON_EXEC% -m %MODULE% %*
 
 PAUSE 5
-GOTO:EOF
-
-:python_module
-CALL %PYTHON_EXEC% -m %MODULE% %*
